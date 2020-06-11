8b7bc60f207cef66cb0ac30cd5119f45649704a8
Flora Petersen <floramarina@comcast.net>
2020-06-11 03:25:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @this_dog_is_named = dog_name
+  end
+  def name
+    @this_dog_is_named
+  end
+end
+
+def bark(dog_name)
+  puts "woof!"
+end
